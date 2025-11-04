4b5dceae35c96ef74c3969ae06bdc966a4871193
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2025-11-04 11:09:28 +0300

    Update SecondNewton.java

--- src/tasks/interpolation/method/SecondNewton.java
+++ src/tasks/interpolation/method/SecondNewton.java
@@ -35,15 +35,15 @@ public class SecondNewton implements InterpolationMethod {
             }
         }
 
-        BigDecimal fractal = new BigDecimal(1); //0!, 1!, 2!, 3!, ..., n!
+        BigDecimal factorial = new BigDecimal(1); //0!, 1!, 2!, 3!, ..., n!
         BigDecimal partWithQ = new BigDecimal(1); //q, q(q+1), q(q+1)(q+2), ..., q(q+1)...(q+n-1)
         BigDecimal Pn = dy.getFirst().getLast(); //Pn = yn + dy<n-1>*q + d2y<n-2>*q(q+1)/2 + d3y<n-3>*q(q+1)(q+2)/6 +...
 
         for(int i = 1; i < points.size(); i++){
-            fractal = fractal.multiply(new BigDecimal(i));
+            factorial = factorial.multiply(new BigDecimal(i));
             partWithQ = partWithQ.multiply(q.add(new BigDecimal(i-1)));
 
-            Pn = Pn.add(dy.get(i).getLast().multiply(partWithQ).divide(fractal, MathContext.DECIMAL128));
+            Pn = Pn.add(dy.get(i).getLast().multiply(partWithQ).divide(factorial, MathContext.DECIMAL128));
         }
 
         return Pn;
