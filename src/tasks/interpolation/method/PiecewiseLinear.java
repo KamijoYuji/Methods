9b29d29ac157111db638a9231e3edf2a6b9fca58
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2025-12-02 23:38:02 +0300

    Update PiecewiseLinear.java

--- src/tasks/interpolation/method/PiecewiseLinear.java
+++ src/tasks/interpolation/method/PiecewiseLinear.java
@@ -4,7 +4,6 @@ import helper.Pair;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
-import java.util.Comparator;
 import java.util.List;
 
 public class PiecewiseLinear implements InterpolationMethod {
@@ -17,14 +16,15 @@ public class PiecewiseLinear implements InterpolationMethod {
 
         for(int i = 0; i < size-1; i++)
             if(x.compareTo(points.get(i).first)>=0 && x.compareTo(points.get(i+1).first)<=0) {
-                BigDecimal x1 = points.get(i).first;
-                BigDecimal x2 = points.get(i+1).first;
-                BigDecimal y1 = points.get(i).second;
-                BigDecimal y2 = points.get(i+1).second;
+                BigDecimal x1 = points.get(i).first;    //x<i>
+                BigDecimal x2 = points.get(i+1).first;  //x<i+1>
+                BigDecimal y1 = points.get(i).second;   //y<i>
+                BigDecimal y2 = points.get(i+1).second; //y<i+1>
 
                 if(x1.equals(x2))
                     throw new IllegalArgumentException("Duplicate x values: " + x1);
 
+                //((y<i+1> - y<i>) / (x<i+1> - x<i>)) * (x - x<i>) + y<i>
                 return (y2.subtract(y1)).divide(x2.subtract(x1), MathContext.DECIMAL128).multiply(x.subtract(x1)).add(y1);
             }
 
