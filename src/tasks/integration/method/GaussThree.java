34a3c71b52e19cc5dd5792c0c5e81dc2856128e4
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2025-12-02 23:52:13 +0300

    Update GaussThree.java

--- src/tasks/integration/method/GaussThree.java
+++ src/tasks/integration/method/GaussThree.java
@@ -13,17 +13,18 @@ public class GaussThree implements IntegrationMethod{
         BigDecimal t2 = new BigDecimal("0");
         BigDecimal t3 = new BigDecimal("0.7745966692414834");
 
-        BigDecimal c1 = new BigDecimal("0.5555555555555556");
-        BigDecimal c2 = new BigDecimal("0.8888888888888889");
-        BigDecimal c3 = new BigDecimal("0.5555555555555556");
+        BigDecimal c1 = new BigDecimal("0.5555555555555556");   //5/9
+        BigDecimal c2 = new BigDecimal("0.8888888888888889");   //8/9
+        BigDecimal c3 = new BigDecimal("0.5555555555555556");   //5/9
 
-        BigDecimal val1 = upperBound.add(lowerBound).divide(BigDecimal.TWO,MathContext.DECIMAL128);
-        BigDecimal val2 = upperBound.subtract(lowerBound).divide(BigDecimal.TWO,MathContext.DECIMAL128);
+        BigDecimal val1 = upperBound.add(lowerBound).divide(BigDecimal.TWO,MathContext.DECIMAL128);     //(b+a)/2
+        BigDecimal val2 = upperBound.subtract(lowerBound).divide(BigDecimal.TWO,MathContext.DECIMAL128);//(b-a)/2
 
-        BigDecimal x1 = val1.add(val2.multiply(t1));
-        BigDecimal x2 = val1.add(val2.multiply(t2));
-        BigDecimal x3 = val1.add(val2.multiply(t3));
+        BigDecimal x1 = val1.add(val2.multiply(t1));    //(b+a)/2 + t<1>(b-a)/2 = (b+a)/2 - t<3>(b-a)/2
+        BigDecimal x2 = val1.add(val2.multiply(t2));    //(b+a)/2 + t<1>(b-a)/2 = (b+a)/2
+        BigDecimal x3 = val1.add(val2.multiply(t3));    //(b+a)/2 + t<3>(b-a)/2 = (b+a)/2 + t<3>(b-a)/2
 
+        //((b-a)/2) * (5f(x<1>)/9 + 8f(x<2>)/9 + 5f(x<3>)/9)
         return val2.multiply(c1.multiply(func.calculate(x1)).add(
                 c2.multiply(func.calculate(x2))).add(
                 c3.multiply(func.calculate(x3)))
