a04a8d76aa019491addb532c5615b875f4d3d72e
KamijoYuji <130177873+KamijoYuji@users.noreply.github.com>
2025-12-02 23:38:56 +0300

    Update Main.java

--- src/tasks/integration/Main.java
+++ src/tasks/integration/Main.java
@@ -15,11 +15,15 @@ public class Main {
         Integration integration1 = new Integration();
         System.out.println(integration1.calculateIntegral(lowerBound1,upperBound1,func1));
         System.out.println("Точное значение: 72");
+        //Решаем: S(6x^5+3x^2)dx = 6S(x^5)dx + 3S(x^2)dx = (6x^6)/6 + (3x^3)/3 + C = x^6 + x^3 + C
+        //Подставляем 2 и 0: (2^6 + 2^3) - (0^6 + 0^3) = (64 + 8) - (0 + 0) = 72 - 0 = 72
 
         BigDecimal lowerBound2 = BigDecimal.ZERO;
         BigDecimal upperBound2 = new BigDecimal("3.14159265358979");
         Integration integration2 = new Integration();
         System.out.println(integration2.calculateIntegral(lowerBound2,upperBound2,func2));
         System.out.println("Точное значение: 2");
+        //Решаем: S(sin(x))dx = -cos(x) + C
+        //Подставляем pi и 0: (-cos(pi)) - (-cos(0)) = (1) - (-1) = 2
     }
 }
